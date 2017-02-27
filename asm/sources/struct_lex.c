18ce55ab33084c53868796661c71189fec538345
punkymaniac <droger@student.42.fr>
2017-02-27 16:05:27 +0100

    gestion structure lex

---
+++ asm/sources/struct_lex.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <struct_lex.h>
+
+static t_lex	*st_get(void)
+{
+	static t_lex		lex = {CMD, 0x00, 0, 0x00};
+
+	return (&lex);
+}
+
+t_lex			*get_lex(void)
+{
+	t_lex		*lex;
+
+	lex = st_get();
+	return (lex->next);
+}
+
+void			new_lex(char *code, e_Type type, int ligne)
+{
+	t_lex		*new;
+	t_lex		*lex;
+
+	lex = st_get();
+	new = (t_lex *)malloc(sizeof(t_lex));
+	new->ligne = ligne;
+	new->type = type;
+	new->code = code;
+	new->next = 0x00;
+	while (lex->next)
+		lex = lex->next;
+	lex->next = new;
+}
+
+void			free_lex(void)
+{
+	t_lex		*lex;
+	t_lex		*temp;
+
+	lex = get_lex();
+	while (lex)
+	{
+		temp = lex;
+		lex = lex->next;
+		temp->code = 0x00;
+		temp->next = 0x00;
+		free(temp);
+	}
+}
