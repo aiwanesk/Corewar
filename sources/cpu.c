055c6dc7065fc2bce52a1cd59e19e95200a5db90
Alexandre IWANESKO <aiwanesk@e2r5p20.42.fr>
2017-02-28 15:18:14 +0100

    addd circle to cpu

--- sources/cpu.c
+++ sources/cpu.c
@@ -5,36 +5,35 @@ t_process				cpu(t_process process, char memory[MEM_SIZE])
 	struct s_arg		arg;
 	int					read_arg[4];
 
-	//TODO faire une globale avec les datas de references dedans plutot que de tout hardcoder
 	if (process.pc == 1 && process.nb_cycle >= 10)
 		apply_live(&process, process.memory);
-	else if (process.pc == 2)
+	else if (process.pc == 2 && process.nb_cycle >= 5)
 		apply_ld(&process, process.memory, arg);
-	else if (process.pc == 3)
+	else if (process.pc == 3 && process.nb_cycle >= 5)
 		apply_st(&process, arg);
-	else if (process.pc == 4)
+	else if (process.pc == 4 && process.nb_cycle >= 10)
 		apply_add(&process, process.memory, arg);
-	else if (process.pc == 5)
+	else if (process.pc == 5 && process.nb_cycle >= 10)
 		apply_and(&process, process.memory, arg);
-	else if (process.pc == 6)
+	else if (process.pc == 6 && process.nb_cycle >= 6)
 		apply_and(&process, process.memory, arg);
-	else if (process.pc == 7)
+	else if (process.pc == 7 && process.nb_cycle >= 6)
 		apply_or(&process, process.memory, arg);
-	else if (process.pc == 8)
+	else if (process.pc == 8 && process.nb_cycle >= 6)
 		apply_xor(&process, process.memory, arg);
-	else if (process.pc == 9)
+	else if (process.pc == 9 && process.nb_cycle >= 20)
 		apply_zjmp(&process, process.memory, arg);
-	else if (process.pc == 10)
+	else if (process.pc == 10 && process.nb_cycle >= 25)
 		apply_ldi(&process, process.memory, arg);
-	else if (process.pc == 11)
+	else if (process.pc == 11 && process.nb_cycle >= 25)
 		apply_sti(&process, arg);
-	else if (process.pc == 12)
+	else if (process.pc == 12 && process.nb_cycle >= 800)
 		apply_fork(&process, arg);
-	else if (process.pc == 13)
+	else if (process.pc == 13 && process.nb_cycle >= 10)
 		apply_lld(&process, process.memory, arg);
-	else if (process.pc == 14)
+	else if (process.pc == 14 && process.nb_cycle >= 50)
 		apply_lldi(&process, process.memory, arg);
-	else if (process.pc == 15)
+	else if (process.pc == 15 && process.nb_cycle >= 1000)
 		apply_lfork(&process, arg);
 	else if (process.pc == 16)
 		apply_aff(&process, process.memory, arg);
