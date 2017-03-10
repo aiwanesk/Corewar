dd2722f26a1da1002c5e8897ded99fc670fa368e
punkymaniac <droger@student.42.fr>
2017-03-10 11:42:55 +0100

    Rewrite apply_or.c

--- VM/sources/apply_or.c
+++ VM/sources/apply_or.c
@@ -20,6 +20,23 @@
 
 void	apply_or(t_process *process, t_env *env)
 {
-	(void) process;
-	(void) env;
+	t_args		args[3];
+	uint32_t	pc;
+	uint32_t	val1;
+	uint32_t	val2;
+	uint32_t	reg;
+
+	decode(args, env->memory[process->pc], env->memory[process->pc + 1]);
+	pc = process->pc + BYPASS_ARG_ENCODE;
+	val1 = get_args(env->memory, pc, args[0].length);
+	pc += args[0].length;
+	val1 = return_value(process, env->memory, args[0], val1);
+	val2 = get_args(env->memory, pc, args[1].length);
+	pc += args[1].length;
+	val2 = return_value(process, env->memory, args[1], val2);
+	reg = get_args(env->memory, pc, args[2].length);
+	process->reg[reg - 1] = (val1 | val2);
+	process->nb_cycle -= 6;
+	process->pc += BYPASS(args, BYPASS_ARG_ENCODE);
+	process->carry = (process->reg[reg - 1] == 0);
 }
