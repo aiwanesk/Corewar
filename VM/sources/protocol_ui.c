46962154b916fe7473fa8523cfd984858ed4870b
Michael BARBARIN <mbarbari@e3r7p13.42.fr>
2017-03-17 17:00:44 +0100

    fix closing window error and memory overflow ui

--- VM/sources/protocol_ui.c
+++ VM/sources/protocol_ui.c
@@ -6,7 +6,7 @@
 /*   By: aiwanesk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 15:49:01 by aiwanesk          #+#    #+#             */
-/*   Updated: 2017/03/17 15:50:54 by aiwanesk         ###   ########.fr       */
+/*   Updated: 2017/03/17 16:56:37 by mbarbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,19 @@ void		protocol_mem(t_env env, t_process process, uint32_t addr)
 	ft_putstr("UI_PROTOCOL MEM ");
 	ft_putnbr_uint32(process.id);
 	ft_putstr("-");
-	ft_putnbr(((a < 0) ? a + MEM_SIZE : a));
+	ft_putnbr(((a < 0) ? a + MEM_SIZE : a % MEM_SIZE));
 	ft_putstr("\nUI_PROTOCOL MEM ");
 	ft_putnbr_uint32(process.id);
 	ft_putstr("-");
-	ft_putnbr(((a < 0) ? a + MEM_SIZE : a) + 1);
+	ft_putnbr(((a < 0) ? a + MEM_SIZE : a % MEM_SIZE) + 1);
 	ft_putstr("\nUI_PROTOCOL MEM ");
 	ft_putnbr_uint32(process.id);
 	ft_putstr("-");
-	ft_putnbr(((a < 0) ? a + MEM_SIZE : a) + 2);
+	ft_putnbr(((a < 0) ? a + MEM_SIZE : a % MEM_SIZE) + 2);
 	ft_putstr("\nUI_PROTOCOL MEM ");
 	ft_putnbr_uint32(process.id);
 	ft_putstr("-");
-	ft_putnbr(((a < 0) ? a + MEM_SIZE : a) + 3);
+	ft_putnbr(((a < 0) ? a + MEM_SIZE : a % MEM_SIZE) + 3);
 	ft_putendl("");
 }
 
